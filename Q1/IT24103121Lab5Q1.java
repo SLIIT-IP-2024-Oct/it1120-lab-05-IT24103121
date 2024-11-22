3aa9bbefa8a1933b87dce6df1d50bf81a805966e
Tharmithan S. <it24103121@my.sliit.lk>
2024-11-22 15:34:17 +0530

    Add files via upload

---
+++ Q1/IT24103121Lab5Q1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class IT24103121Lab5Q1 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        
+        // Prompt the user to enter the first integer
+        System.out.println("Enter the first integer:");
+        int F_num = num.nextInt();
+        
+      
+        System.out.println("Enter the second integer:");
+        int S_num = num.nextInt();
+        
+      
+        System.out.println("Enter the third integer:");
+        int T_num = num.nextInt();
+        
+        
+        int large = F_num;
+        if (S_num > large) {
+            large = S_num;
+        }
+        if (T_num > large) {
+            large = T_num;
+        }
+        
+     
+        int Small = F_num;
+        if (S_num < Small) {
+            Small = S_num;
+        }
+        if (T_num < Small) {
+            Small = T_num;
+        }
+
+      
+        System.out.println("User entered numbers are: " + F_num + ", " + S_num + ", " + T_num);
+        System.out.println("The Smallest number is: " + Small); // Correct concatenation with +
+        System.out.println("The Largest number is: " + large); // Correct concatenation with +
+    }
+}
